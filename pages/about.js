11ba7e1610f6ab20b60c4b16d0c7fa290d71d919
habibirrahman <habibirrahman18@gmail.com>
2020-06-02 19:32:20 +0700

    Router & NProgress

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout title="About">
+    <p>A JavaScript Programmer</p>
+    <img src="/static/js-logo.png" alt="JavaScript" height="200px" />
+  </Layout>
+);
---
+++ pages/hireme.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+
+const HireMe = () => (
+  <Layout title="Hire Me">
+    <p>
+      You can hire me at{" "}
+      <a href="mailto:arief.habibirrahman@gmail.com">
+        arief.habibirrahman@gmail.com
+      </a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
